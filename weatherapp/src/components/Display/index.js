1a965653df6e81f81c9a3902e476e8e40c4600f9
Kyle <kylejordanwilliams@gmail.com>
2023-04-21 14:43:50 +0100

    Display functiional

--- weatherapp/src/components/Display/index.js
+++ weatherapp/src/components/Display/index.js
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react";
 
-//const API_KEY = 'ffc9d5a9b44b42e88249b54818508ef8';
-
 function WeatherDisplay({ city_name }) {
-  const [weather, setWeather] = useState({});
+  const [weatherNow, setWeatherNow] = useState({});
 
   useEffect(() => {
     async function getWeather() {
@@ -11,22 +9,32 @@ function WeatherDisplay({ city_name }) {
         `http://api.weatherbit.io/v2.0/current?city=${city_name}&key=ffc9d5a9b44b42e88249b54818508ef8`
       );
       const weatherData = await response.json();
-      setWeather(weatherData.data[0]);
+      setWeatherNow(weatherData.data[0]);
     }
     getWeather();
-  }, []);
+  }, [city_name]);
+
+  // Add an if statement to check cloud cover
+  let cloudCover;
+  if (weatherNow.clouds < 50) {
+    cloudCover = "cloudy";
+  } else {
+    cloudCover = "Sunny";
+  }
 
   return (
     <div>
-      <h1>{weather.city_name}</h1>
-      <h2>{weather.temp} °c </h2>
+      <h1>
+        {weatherNow.city_name}, {weatherNow.country_code}
+      </h1>
+      <h2>{Math.floor(weatherNow.temp)} °C </h2>
+      <h3>Chance of rain: {Math.floor(weatherNow.precip)} %</h3>
       <div>
-        <p>Clouds: {weather.c} </p>
-        <p>Humidity: {weather.rh}</p>
-        <p>Wind Speed: {weather.wind_spd}</p>
-        <p>UV: {weather.uv}</p>
+        <p>Clouds: {cloudCover}</p>
+        <p>Humidity: {weatherNow.rh} %</p>
+        {/* {<p>Wind Speed: {weatherNow.wind_spd.toFixed(1)} km/h</p>} */}
+        <p>UV index: {weatherNow.uv}</p>
       </div>
-      <p>Discription {weather.weather[0]}</p>
     </div>
   );
 }
